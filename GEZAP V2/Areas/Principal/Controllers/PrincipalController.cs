6b4743ce9e08a0167d8f5721ecca6b5e7fe3f456
juliethaleja <juliethaleja2000@gmail.com>
2020-11-20 21:00:33 -0500

    Agregar archivos de proyecto.

---
+++ GEZAP V2/Areas/Principal/Controllers/PrincipalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GEZAP_V2.Areas.Principal.Controllers
+{
+    [Area("Principal")]
+    public class PrincipalController : Controller
+    {
+        public IActionResult Principal()
+        {
+            return View();
+        }
+    }
+}
